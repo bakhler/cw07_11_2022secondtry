1939f330933e22aef00cd909720c39e79ffbdf2e
bakhler <thehenridit@gmail.com>
2022-11-08 17:23:20 +0200

    Add example conversion

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-let number1 = "12"; //2b
-number1 = "123456";
-number1 = 78; //4b
-number1 = number1 = +45; //4b
-//number1 = 4n; //8b
-let nubmerBig = 13n; //bigint
+let number1 = 12;
+let number2 = "12";
+
+console.log(number1 + number2); 
+console.log(number1 + Number(number2)); 
+
+
+number1 = 5;
+number1 = "qwer";
+
+console.log(number1 * number2);
