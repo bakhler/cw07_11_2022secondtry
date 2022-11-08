bf5dcfe5d8a406d31e61f4483c90ad5fe9fd8961
bakhler <thehenridit@gmail.com>
2022-11-08 15:37:57 +0200

    Add const and let

---
+++ index.js
@@ -0,0 +1,15 @@
+let nubmer1;
+
+console.log(nubmer1);
+number1 = 12;
+console.log(number1);
+let userFirstName;
+
+const userLastName = "Pitt";
+
+userFirstName = "Tom";
+//userFirstName = "Bred";
+
+userLastName = "Cruse";
+
+console.log(userFirstName, userLastName);
