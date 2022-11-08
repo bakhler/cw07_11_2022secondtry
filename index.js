97e945bc57e4dce02d624350c257f8fe211fac02
bakhler <thehenridit@gmail.com>
2022-11-08 15:47:51 +0200

    Add bigint

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-let nubmer1;
-
-console.log(nubmer1);
-number1 = 12;
-console.log(number1);
-let userFirstName;
-
-const userLastName = "Pitt";
-
-userFirstName = "Tom";
-//userFirstName = "Bred";
-
-userLastName = "Cruse";
-
-console.log(userFirstName, userLastName);
+let number1 = 12;
+let nubmerBig = 13n; //bigint
