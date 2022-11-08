ab17a32f2e3209f0e01ae0a9aff9474afeb638b3
bakhler <thehenridit@gmail.com>
2022-11-08 16:11:49 +0200

    Add text to index js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let number1 = '12'; //2b
-nubmer1 = 78; //4b
+let number1 = "12"; //2b
+number1 = "123456";
+number1 = 78; //4b
 number1 = number1 = +45; //4b
-number1 = 4n; //8b
+//number1 = 4n; //8b
 let nubmerBig = 13n; //bigint
